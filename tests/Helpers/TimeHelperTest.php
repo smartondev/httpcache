00a38aa373714595bd7762736599a86fad5c0f7a
Márton Somogyi <kamarton@gmail.com>
2024-08-24 02:47:02 +0200

    test: test added

--- tests/Helpers/TimeHelperTest.php
+++ tests/Helpers/TimeHelperTest.php
@@ -22,4 +22,13 @@ class TimeHelperTest extends TestCase
         $this->assertSame(31536000, TimeHelper::durationToSeconds(years: 1));
         $this->assertSame(3722, TimeHelper::durationToSeconds(seconds: 2, minutes: 2, hours: 1));
     }
+
+    public function testToTimestamp(): void
+    {
+        $this->assertSame(0, TimeHelper::toTimestamp(0));
+        $this->assertSame(1634025600, TimeHelper::toTimestamp('2021-10-12T08:00:00Z'));
+        $this->assertSame(1800000000, TimeHelper::toTimestamp('2027-01-15T08:00:00Z'));
+        $this->assertSame(2147483647, TimeHelper::toTimestamp('2038-01-19T03:14:07Z'));
+        $this->assertSame(1634025600, TimeHelper::toTimestamp(new \DateTime('2021-10-12T08:00:00Z')));
+    }
 }
