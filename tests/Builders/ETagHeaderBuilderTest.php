92211a0d59f3de06235d6f7b8dcedb665bcb2894
Márton Somogyi <kamarton@gmail.com>
2024-08-25 06:32:44 +0200

    test: more test added

--- tests/Builders/ETagHeaderBuilderTest.php
+++ tests/Builders/ETagHeaderBuilderTest.php
@@ -30,7 +30,7 @@ class ETagHeaderBuilderTest extends TestCase
         $this->assertSame($expectedHeaders, $builder->toHeaders());
     }
 
-    public static function dataProviderWithComputedEtag()
+    public static function dataProviderComputedEtag(): array
     {
         return [
             ['content123', 'md5', false, ['etag' => '"' . md5('content123') . '"']],
@@ -44,13 +44,24 @@ class ETagHeaderBuilderTest extends TestCase
         ];
     }
 
-    #[DataProvider('dataProviderWithComputedEtag')]
-    public function testWithComputedEtag(mixed $data, callable $func, bool $weekEtag, array $expectedHeaders): void
+    #[DataProvider('dataProviderComputedEtag')]
+    public function testComputedEtag(mixed $data, callable $func, bool $weekEtag, array $expectedHeaders): void
     {
         $builder = (new ETagHeaderBuilder())
             ->computedETag($data, $func);
         if ($weekEtag) {
             $builder->weekETag();
+        }
+        $this->assertSame($expectedHeaders, $builder->toHeaders());
+    }
+
+    #[DataProvider('dataProviderComputedEtag')]
+    public function testWithComputedEtag(mixed $data, callable $func, bool $weekEtag, array $expectedHeaders): void
+    {
+        $builder = (new ETagHeaderBuilder())
+            ->withComputedETag($data, $func);
+        if ($weekEtag) {
+            $builder = $builder->withWeekETag();
         }
         $this->assertSame($expectedHeaders, $builder->toHeaders());
     }
@@ -64,5 +75,57 @@ class ETagHeaderBuilderTest extends TestCase
         $builder = (new ETagHeaderBuilder())
             ->etag('    ');
         $this->assertNull($builder->getETag());
+    }
+
+    public function testIsNotEmptyETag(): void
+    {
+        $builder = (new ETagHeaderBuilder())
+            ->etag('123456');
+        $this->assertTrue($builder->isNotEmpty());
+
+        $builder = (new ETagHeaderBuilder())
+            ->etag('    ');
+        $this->assertFalse($builder->isNotEmpty());
+    }
+
+    public function testResetETag(): void
+    {
+        $builder = (new ETagHeaderBuilder())
+            ->etag('123456');
+        $this->assertTrue($builder->isNotEmpty());
+        $this->assertFalse($builder->withoutETag()->isNotEmpty());
+        $this->assertTrue($builder->isNotEmpty());
+        $builder->resetETag();
+        $this->assertFalse($builder->isNotEmpty());
+    }
+
+    public function testResetWeekETag(): void
+    {
+        $builder = (new ETagHeaderBuilder())
+            ->etag('123456')
+            ->weekETag();
+        $this->assertSame('W/"123456"', $builder->getETag());
+        $this->assertSame('"123456"', $builder->withoutWeekETag()->getETag());
+        $this->assertSame('W/"123456"', $builder->getETag());
+        $builder->resetWeekETag();
+        $this->assertSame('"123456"', $builder->getETag());
+    }
+
+    public function testEmpty(): void
+    {
+        $builder = new ETagHeaderBuilder();
+        $this->assertSame([], $builder->toHeaders());
+    }
+
+    public function testToString(): void
+    {
+        $builder = (new ETagHeaderBuilder())
+            ->etag('123456');
+        $this->assertSame('"123456"', (string)$builder);
+        $this->assertSame($builder->getETag(), (string)$builder);
+
+        $builder->weekETag();
+        $this->assertSame('W/"123456"', (string)$builder);
+        $this->assertSame($builder->getETag(), (string)$builder);
     }
 }
