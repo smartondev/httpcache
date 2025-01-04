ec062facfa8bc3ee462a91cfcabac6f600fc1715
Márton Somogyi <kamarton@gmail.com>
2025-01-04 08:10:12 +0100

    enh: improve ETagHeaderBuilder coverage

--- tests/Builders/ETagHeaderBuilderTest.php
+++ tests/Builders/ETagHeaderBuilderTest.php
@@ -50,6 +50,44 @@ it('computed etag', function (mixed $data, callable $compute, array $expectedHea
     ],
 ]);
 
+it('with computed etag', function (mixed $data, callable $compute, array $expectedHeaders) {
+    $builder = (new ETagHeaderBuilder())
+        ->etag('123456');
+    $builder2 = $builder->withComputedETag($data, $compute);
+    expect($builder->toHeaders())->toBe(['etag' => '"123456"'])
+        ->and($builder2->toHeaders())->toBe($expectedHeaders);
+})->with([
+    'md5' => [
+        'content123',
+        'md5',
+        ['etag' => '"' . md5('content123') . '"'],
+    ],
+    'sha1' => [
+        'content789',
+        'sha1',
+        ['etag' => '"' . sha1('content789') . '"'],
+    ],
+    'int multiple' => [
+        2,
+        fn($d) => strval($d * 11),
+        ['etag' => '"22"'],
+    ],
+]);
+
+it('computed etag with invalid return value', function (mixed $return) {
+    $builder = (new ETagHeaderBuilder());
+    expect(fn() => $builder->computedETag('content123', fn() => $return))->toThrow(
+        exception: InvalidArgumentException::class,
+        message: 'ETag must be a string or null',
+    );
+})->with([
+    'array' => [[]],
+    'object' => [new stdClass()],
+    'int' => [123],
+    'float' => [123.456],
+    'bool' => [true],
+]);
+
 it('week etag', function () {
     $builder = (new ETagHeaderBuilder())
         ->etag('123456')
@@ -60,13 +98,15 @@ it('week etag', function () {
 it('is empty', function () {
     $builder = (new ETagHeaderBuilder())
         ->etag('123456');
-    expect($builder->isEmpty())->toBeFalse();
+    expect($builder->isEmpty())->toBeFalse()
+        ->and($builder->withEtag(null)->isEmpty())->toBeTrue();
 });
 
 it('is not empty', function () {
     $builder = (new ETagHeaderBuilder())
         ->etag(null);
-    expect($builder->isEmpty())->toBeTrue();
+    expect($builder->isNotEmpty())->toBeFalse()
+        ->and($builder->withEtag('123456')->isNotEmpty())->toBeTrue();
 });
 
 it('reset etag', function () {
@@ -98,6 +138,22 @@ it('with weak etag', function () {
         ->and($builder->toHeaders())->toBe(['etag' => '"123456"']);
 });
 
+it('without week etag', function () {
+    $builder = (new ETagHeaderBuilder())
+        ->etag('123456')
+        ->weekETag();
+    expect($builder->withoutWeekETag()->toHeaders())->toBe(['etag' => '"123456"']);
+});
+
+it('reset week etag', function () {
+    $builder = (new ETagHeaderBuilder())
+        ->etag('123456')
+        ->weekETag();
+    expect($builder->toHeaders())->toBe(['etag' => 'W/"123456"']);
+    $builder->resetWeekETag();
+    expect($builder->toHeaders())->toBe(['etag' => '"123456"']);
+});
+
 it('initial empty', function () {
     $builder = new ETagHeaderBuilder();
     expect($builder->toHeaders())->toBeEmpty();
@@ -115,4 +171,9 @@ it('get etag', function () {
         ->etag('123456');
     expect($builder->getETag())->toBe('"123456"')
         ->and($builder->withWeekETag()->getETag())->toBe('W/"123456"');
+});
+
+it('get etag if not set', function () {
+    $builder = new ETagHeaderBuilder();
+    expect($builder->getETag())->toBeNull();
 });
